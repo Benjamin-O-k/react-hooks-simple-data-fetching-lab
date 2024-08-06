15dbfcf31e72319a09fce67abd5c7e23a07353b9
Benjamin Okello <benjamin.okello@student.moringaschool.com>
2024-08-07 02:43:29 +0300

    created App.js component

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
 // create your App component here
+import React,{useEffect, useState} from 'react';
+
+export default function App (){
+    const [image, setimage] = useState('');
+
+    useEffect(() =>{
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response => response.json())
+        .then(data => setimage(data.message))
+    })
+
+    if(!image){
+        return <h1>Loading...</h1>
+    }
+    return (
+        <div>
+            <img src={image} alt= "A Random Dog"/>
+        </div>
+    );
+}
